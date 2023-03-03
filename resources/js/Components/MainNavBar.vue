4d6f815e13df23bc04993f41f8b5364bace9d291
Amin <sabeurmohammedamin@gmail.com>
2023-03-03 23:20:24 +0100

    ..

--- resources/js/Components/MainNavBar.vue
+++ resources/js/Components/MainNavBar.vue
@@ -3,48 +3,46 @@
 import { computed, ref, getCurrentInstance } from "vue";
 import { usePage } from '@inertiajs/vue3'
 
-// Declaring and using emits
-const emit = defineEmits(['navDrawerSwitcher', 'cartDrawerSwitcher', 'toggleTheme']);
+  // Declaring and using emits
+  const emit = defineEmits(['navDrawerSwitcher', 'cartDrawerSwitcher', 'toggleTheme']);
 
-// TOOGLE AND ACTIVE THE SHP BTN
-const activeButtonShop = computed(() => {
-  return route('home')+usePage().url === route('component.build') ? true : false;
-});
+  // TOOGLE AND ACTIVE THE SHP BTN
+  const activeButtonShop = computed(() => {
+    return route('home')+usePage().url === route('component.build') ? true : false;
+  });
 
-// CHECKING FOR THE THEME USED
-let internalInstance = getCurrentInstance();
-let isLightTheme = computed(() => {
+  // CHECKING FOR THE THEME USED
+  let internalInstance = getCurrentInstance();
+  let isLightTheme = computed(() => {
   const GLOBAL_VUETIFY = internalInstance.ctx.$vuetify;
   //console.log(internalInstance.ctx.$vuetify);
   return GLOBAL_VUETIFY.theme.name === "light" ? true : false;
-});
-let themeIconDark = ref("mdi-moon-waning-crescent");
-let themeIconLight = ref("mdi-weather-sunny");
+  });
+  let themeIconDark = ref("mdi-moon-waning-crescent");
+  let themeIconLight = ref("mdi-weather-sunny");
 
-
-function navigationDrawerToggel() {
-  emit("navDrawerSwitcher");
-}
-function cartDrawerToggel() {
-  emit("cartDrawerSwitcher");
-}
-function darkLightTheme() {
-  emit("toggleTheme");
-}
+  function navigationDrawerToggel() {
+    emit("navDrawerSwitcher");
+  }
+  function cartDrawerToggel() {
+    emit("cartDrawerSwitcher");
+  }
+  function darkLightTheme() {
+    emit("toggleTheme");
+  }
 </script>
 
-
 <template>
-    <v-app-bar elevation="0" height="54" border>
+  <v-app-bar elevation="0" height="54" border>
       <!--THIS IS FOR MENU-->
-      <v-app-bar-nav-icon
-        rounded="lg"
-        @click="navigationDrawerToggel"
-        tabindex="5"
-      >
+    <v-app-bar-nav-icon
+      rounded="lg"
+      @click="navigationDrawerToggel"
+      tabindex="5"
+    >
       </v-app-bar-nav-icon>
   
-      <!--THIS IS FOR LOGO-->
+    <!--THIS IS FOR LOGO-->
     <Link :href="route('home')">
       <v-btn
       :active="false"
@@ -59,59 +57,57 @@ function darkLightTheme() {
       </v-btn>
     </Link>
 
-  
-      <!--THIS IS FOR SHOP BTN-->
-      <Link :href="route('component.build')">
-        <v-btn
-          :active="activeButtonShop"
-          class="mx-2"
-          rounded="lg"
-          variant="text"
-          tabindex="6"
-        >
-        All Categories
-        </v-btn>
-      </Link>
-
-  
-      <v-spacer />
+    <!--THIS IS FOR SHOP BTN-->
+    <Link :href="route('component.build')">
       <v-btn
+        :active="activeButtonShop"
+        class="mx-2"
+        rounded="lg"
+        variant="text"
         tabindex="6"
-        class="text-body-2 pa-0 ma-0 text-center"
-        variant="plain"
-        @click="darkLightTheme"
-        size="x-small"
       >
-        <v-icon
-          v-if="isLightTheme"
-          :icon="themeIconDark"
-          style="transform: rotateZ(-45deg)"
-        ></v-icon>
-        <v-icon v-else :icon="themeIconLight" color="yellow"></v-icon>
+        All Categories
       </v-btn>
+    </Link>
+
+    <v-spacer />
+    <v-btn
+      tabindex="6"
+      class="text-body-2 pa-0 ma-0 text-center"
+      variant="plain"
+      @click="darkLightTheme"
+      size="x-small"
+    >
+      <v-icon
+        v-if="isLightTheme"
+        :icon="themeIconDark"
+        style="transform: rotateZ(-45deg)"
+      ></v-icon>
+      <v-icon v-else :icon="themeIconLight" color="yellow"></v-icon>
+    </v-btn>
   
       <!--THIS IS FOR SEARCH-->
-      <v-btn
-        id="btn_search_overlay"
-        rounded="lg"
-        class="mx-2"
-        variant="text"
-        icon="mdi-magnify"
-        color="black-lighten-2"
-        tabindex="7"
-      ></v-btn>
+    <v-btn
+      id="btn_search_overlay"
+      rounded="lg"
+      class="mx-2"
+      variant="text"
+      icon="mdi-magnify"
+      color="black-lighten-2"
+      tabindex="7"
+    ></v-btn>
   
-      <!--THIS IS FOR CART SHOPPING-->
-      <v-divider vertical inset class="ml-2"></v-divider>
-      <v-btn
-        id="btn_cart"
-        rounded="lg"
-        variant="outlined"
-        class="mx-2"
-        icon="mdi-cart-variant"
-        color="black-lighten-2"
-        @click="cartDrawerToggel"
-        tabindex="8"
-      ></v-btn>
-    </v-app-bar>
-  </template>
+    <!--THIS IS FOR CART SHOPPING-->
+    <v-divider vertical inset class="ml-2"></v-divider>
+    <v-btn
+      id="btn_cart"
+      rounded="lg"
+      variant="outlined"
+      class="mx-2"
+      icon="mdi-cart-variant"
+      color="black-lighten-2"
+      @click="cartDrawerToggel"
+      tabindex="8"
+    ></v-btn>
+  </v-app-bar>
+</template>
